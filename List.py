6f41e3c632bc75d076d91e4684007de9d9697e42
angelav011301 <42008429+angelav011301@users.noreply.github.com>
2018-09-04 12:03:45 -0400

    Add files via upload

---
+++ List.py
@@ -0,0 +1,44 @@
+#Places = ["London", "Korea", "Japan", "Flordia", "California", "Germany", "Jamacia"]
+#Numbers = [1, 2, 3, 4 ,5 ,6 ,7]
+#Fruit = ["Apple", "Orange", "Strawberry", "Pineapple", "Banana", "Kiwi"]
+
+#print (len(Places))
+#print (len(Numbers))
+#print (len(Fruit))
+
+#print(Places[2])
+
+#for letter in Fruit:
+    #print(letter)
+#print(Places)
+#Places.append("berry")
+#print("Testing append", Places)
+
+#print(Places)
+#Places.insert(2,"NJ")
+#print("testing insert", Places)
+
+import random
+FirstNames = ["Audrey", "Becky", "Christopher", "Dave", "Ethan", "Felicia", "George", "Hannah", "Ian", "Jill", "Kyle", "Larry", "Manny", "Nancy", "Oscar", "Patricia", ]
+LastNames= ["Johnson", "Baker", "Pratt", "Grace", "Vazquez", "Hutchinson"]
+print (random.choice(FirstNames))
+print(random.choice(LastNames))
+
+#Menu Genorator
+print ("Tonight you will be having...")
+Main = ["Chicken", "Fish", "Steak", "Crab", "Lobster", "Pasta"]
+print (random.choice(Main))
+print ("with a side of")
+Side = ["Mashed Potatoes", "Corn", "Soup", "Salad", "Fries"]
+print(random.choice(Side))
+print ("will be drinking")
+Drink = ["Coke", "Pepsi", "Water", "Lemonade", "Ice Tea", "Coffee"]
+print (random.choice(Drink))
+print ("and have")
+Dessert = ["Cheese Cake", "Cookie", "Ice Cream", "Donut"]
+print(random.choice(Dessert))
+
+#print (random.choice(Main))
+#print(random.choice(Side))
+#print (random.choice(Drink))
+#print(random.choice(Dessert))
